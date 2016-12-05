0ffe7c89d072d2265d9f0b13208df869acb24b2d
niels.van.enckevort <nielsvanenckevort@hotmail.com>
2016-12-05 17:40:35 +0100

    Verbetering vakken
    vakken verbeteringen

--- Logboek-docenten-template/Admin-panel/vakken.php
+++ Logboek-docenten-template/Admin-panel/vakken.php
@@ -18,42 +18,21 @@
 		<div class="row">
 			<div class="col-md-8">
 				<div class="panel panel-default">
-					<div class="panel-heading">Meldingen</div>
+					<div class="panel-heading">
+                        Meldingen
+                    </div>
 					<div class="panel-body">
-						<form class="form-horizontal" action="" method="post">
-							<fieldset>
-								<!-- Name input-->
-								<div class="form-group">
-									<div class="col-md-9">
-
-                                    </div>
-								</div>
-
-								<!-- Email input-->
-								<div class="form-group">
-									<label class="col-md-3 control-label" for="email">Your E-mail</label>
-									<div class="col-md-9">
-										<input id="email" name="email" type="text" placeholder="Your email" class="form-control">
-									</div>
-								</div>
-
-								<!-- Message body -->
-								<div class="form-group">
-									<label class="col-md-3 control-label" for="message">Your message</label>
-									<div class="col-md-9">
-										<textarea class="form-control" id="message" name="message" placeholder="Please enter your message here..." rows="5"></textarea>
-									</div>
-								</div>
-
-								<!-- Form actions -->
-								<div class="form-group">
-									<div class="col-md-12 widget-right">
-										<button type="submit" class="btn btn-default btn-md pull-right">Submit</button>
-									</div>
-								</div>
-							</fieldset>
-						</form>
-					</div>
+                        <div class="col-md-9">
+                            <h3>
+                                Uw gekozen vak: <!-- echo gekozen vak -->
+                            </h3>
+                        </div>
+                        <div class="col-md-9">
+                            <h3>
+                                Uw gekozen klas: <!-- echo gekozen klas -->
+                            </h3>
+                        </div>
+                    </div>
 				</div>
 
 				<div class="panel panel-default chat">
